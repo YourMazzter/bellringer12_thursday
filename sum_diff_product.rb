431716640e326d91e7de0ae9ba4043c91242f3a7
Katrice White <kwhite@chicagotechacademy.org>
2013-11-08 15:04:43 -0600

    what

--- sum_diff_product.rb
+++ sum_diff_product.rb
@@ -1,8 +1,4 @@
-puts "Please insert a number"
-answer = gets.chomp
-puts "Place another number"
-second = gets.chomp
-
-puts answer.to_i * second.to_i
-puts answer.to_i - second.to_i
-puts answer.to_i + second.to_i
+def method(punk,boss)
+puts "into interger"
+punk = gets.chomp.to_i
+boss = gets.chomp.to_i
